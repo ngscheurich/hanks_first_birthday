8688237c476d0b67a04352ee91600180637be77f
Nicholas Scheurich <nick@scheurich.me>
2016-10-12 16:32:55 -0500

    Add Timex

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule HanksFirstBirthday.Mixfile do
   def application do
     [mod: {HanksFirstBirthday, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :ex_machina]]
+                    :phoenix_ecto, :postgrex, :ex_machina, :timex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,8 @@ defmodule HanksFirstBirthday.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:ex_machina, "~> 1.0"}]
+     {:ex_machina, "~> 1.0"},
+     {:timex, "~> 3.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
