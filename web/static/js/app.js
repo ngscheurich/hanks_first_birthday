8fa4542902af4f6411c82b947de4784c0b341c09
Nicholas Scheurich <nick@scheurich.me>
2016-10-13 09:41:36 -0500

    Hook up form to notes API

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,44 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import 'phoenix_html';
+import Vue from 'vue/dist/vue.js';
+import VueResource from 'vue-resource';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+Vue.use(VueResource);
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+const vm = new Vue({
+  el: '#app',
 
-// import socket from "./socket"
+  data: {
+    notes: [],
+    payload: {
+      note: {
+        from: '',
+        body: ''
+      }
+    }
+  },
+
+  created: function () {
+    this.fetchNotes();
+  },
+
+  methods: {
+    fetchNotes: function () {
+      console.log('Fetching…');
+      this.$http.get('/api/notes').then((res) => {
+        this.notes = res.body.notes;
+      }, (res) => {
+        console.error(`Error: ${res.body}`);
+      });
+    },
+    createNote: function () {
+      console.log('Creating…');
+      const payload = JSON.stringify(this.payload);
+      this.$http.post('/api/notes', payload).then((res) => {
+        this.fetchNotes();
+        this.payload = {note: {from: '', body: ''}};
+      }, (res) => {
+        console.error(`Error: ${res.body}`);
+      });
+    }
+  }
+});
