c47bed52f020d363f218572e5d58f048b4116161
Nicholas Scheurich <nick@scheurich.me>
2016-10-13 13:52:58 -0500

    Fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :hanks_first_birthday, HanksFirstBirthday.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "sleepy-coast-78221.heroku.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :logger, level: :info
