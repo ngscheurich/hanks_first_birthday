1cb830c15a09387231417164c6113c5bc41f7439
Nicholas Scheurich <nick@scheurich.me>
2016-10-13 13:58:18 -0500

    Remove Timex

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,12 +1,7 @@
 defmodule HanksFirstBirthday.PageView do
   use HanksFirstBirthday.Web, :view
 
-  use Timex
-
   def days_old do
-    now = Timex.now("America/Chicago")
-    bday = Timex.parse!("2015-10-25T14:30-06:00", "{ISO:Extended}")
-    Interval.new(from: bday, until: now)
-    |> Interval.duration(:days)
+    ""
   end
 end
