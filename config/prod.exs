4923668fa49f441667772b06222c71c3efef4d68
Nicholas Scheurich <nick@scheurich.me>
2016-10-13 14:44:30 -0500

    Use SSL

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :hanks_first_birthday, HanksFirstBirthday.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "sleepy-coast-78221.heroku.com", port: 80],
+  url: [scheme: "https", host: "sleepy-coast-78221.heroku.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -11,5 +12,6 @@ config :logger, level: :info
 config :hanks_first_birthday, HanksFirstBirthday.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
